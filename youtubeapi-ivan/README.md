967665a1d6732b56c15693fd2c00c6aa0b0a7a85
Victor <vicfaraujo@gmail.com>
2025-01-01 23:55:17 -0300

    videos dez-2024

--- youtubeapi-ivan/README.md
+++
@@ -1,3 +0,0 @@
-#Introduction
-
-This project uses the youtube API to get the title and view count from a video using its id. The data is stored in a csv file.
--- youtubeapi-ivan/videos.py
+++ youtubeapi-ivan/videos.py
@@ -15,7 +15,7 @@ from googleapiclient.discovery import build
 scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
 
 
-def main():
+def main(n):
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
@@ -24,44 +24,49 @@ def main():
     apiver = "v3"
     key = "YOUR_CLIENT_SECRET_FILE.json"
 
-    #Gets the developer key from an environment variable. You need to set one using your own developer key.
+    # Gets the developer key from an environment variable. You need to set one using your own developer key.
     youtube = build(apiname, apiver,
-    developerKey=os.getenv("YOUTUBE_DEVELOPER_KEY"))
+                    developerKey="AIzaSyDYC18PLsHQguqzhKCD0GOabhe49qK2p5I")
+    # developerKey=os.getenv("YOUTUBE_DEVELOPER_KEY"))
 
-
-    #Builds the request using video ids.
+    # Builds the request using video ids.
+    if n == 0:
+        videoIds = "yHF-9etoZyA,ymknJKeFoLY,zi_IOv7Oc_o,rSH-2gz_aIs,aNlZfBREjSs,cMJZtGIhCqE,VQZkcCMVTj4,AczvqfirMRM,yv8HUagtV_w,RG_pMMxf6rk,yPzP8AllF7A,3x3EETpfSZI,8rYPoLvHW_A,tFeJyx3vCZs,Wp7xf1BQXBA,hzZmoHU7cN0,OxxMMlK7Hhw,08yUsTWwSP8,X9kHwMw8cio,NuHoGT23VlQ,E1hFLpqf1AM,_3UAXQcrbGM,Tpm7X17oKa4,dCBULlO0gDQ,_3UAXQcrbGM,He1AiTaEyPU,pzPNtYeRTvg,Rm6iIHfh-7o,iN-uXRLJ-58,H0U9l3q4p68,lmHKZoPY-g0,zzZmzn3ynhc,rkw8BuCQSbw,29aAaRHGb7E,RayhKM1NsIc,0iW6s4XGQS0,hF58Z87b-ro,n2gNkXm6BhY,rNrHSaI9QL8,oYAWgJwuEOM,9rhWvc_ttJI,A3vIAguMG6Q"
+        name = "videos-aperipe.csv"
+    elif n == 1:
+        videoIds = "tPJzm39kmoQ,fiGwu1aXNfw,VQZkcCMVTj4,mYgcEcJO4RQ,C_21ghi6grE,t8QAkme6m8M,O3sp5Qphl8A,QwCbJSV49nk,7CccNuwDjfY,khWe-Zhx2HY,U1_limKscyE,aSPua88KoDU,az4Am6P3ksY,9n59NlPpWWE,NgkeoiRCDbo,CCIKouQ1ttc,ZQoIvLf-Kb8,a5TjKzcdGuM,eu-HbrDVn4Y"
+        name = "videos-nco.csv"
+    elif n == 2:
+        videoIds = "EMm1nPzChyI,EfjUDCPma2k,zyO3L6OZp84,xOFKSxXx_Dc,tXntAHuKaaU,FW8XRUXhcZI,elXImOSUr0Q,0TAyrBibNFQ,4OgVyKCjeKI,g7cB5MExYuI,ZLLoBaxp3Ok,tUQct2S46pc,9p_6pZCYVok,w-ZVY2mm6GU,a7KlRMEgOvY,lqbS2fVaZCE,qvfHyXuiB8w,dGYrMh-VhAw,iMF7XbefzXg,HSIaTLgr2bU"
+        name = "videos-dctv.csv"
+    elif n == 3:
+        videoIds = "3bSS6i6wcfg,0TYWAuagax4,JFJeGlxBhvg"
+        name = "videos-novos.csv"
 
     request = youtube.videos().list(
+
         part="statistics,snippet",
-        #Vídeos iniciais
-        #id="yHF-9etoZyA,ymknJKeFoLY,cMJZtGIhCqE,E1hFLpqf1AM,8rYPoLvHW_A,AczvqfirMRM,RG_pMMxf6rk,zi_IOv7Oc_o,yPzP8AllF7A,aNlZfBREjSs,hzZmoHU7cN0,yv8HUagtV_w,Tpm7X17oKa4,NuHoGT23VlQ,3x3EETpfSZI,Wp7xf1BQXBA,dCBULlO0gDQ,OxxMMlK7Hhw,08yUsTWwSP8,Rm6iIHfh-7o,lmHKZoPY-g0,rkw8BuCQSbw,zzZmzn3ynhc,_3UAXQcrbGM,pzPNtYeRTvg,S3YrDEW-l4Q,X9kHwMw8cio,29aAaRHGb7E,H0U9l3q4p68,He1AiTaEyPU,RayhKM1NsIc,hF58Z87b-ro,0iW6s4XGQS0,rNrHSaI9QL8,oYAWgJwuEOM,9rhWvc_ttJI,A3vIAguMG6Q,n2gNkXm6BhY,rSH-2gz_aIs,iN-uXRLJ-58"
-        #Vídeos PCO
-        #id="tPJzm39kmoQ,fiGwu1aXNfw,t8QAkme6m8M,QwCbJSV49nk,O3sp5Qphl8A,7CccNuwDjfY,Rw57unxzcGs,khWe-Zhx2HY,U1_limKscyE,az4Am6P3ksY,9n59NlPpWWE,NgkeoiRCDbo,CCIKouQ1ttc,ZQoIvLf-Kb8,a5TjKzcdGuM,eu-HbrDVn4Y,aSPua88KoDU,mYgcEcJO4RQ"    )
-        #Videos DCTV
-        id="xOFKSxXx_Dc,EMm1nPzChyI,tXntAHuKaaU,zyO3L6OZp84,FW8XRUXhcZI,ZLLoBaxp3Ok,w-ZVY2mm6GU,elXImOSUr0Q,qvfHyXuiB8w,a7KlRMEgOvY,4OgVyKCjeKI,0TAyrBibNFQ,lqbS2fVaZCE,g7cB5MExYuI,EfjUDCPma2k,tUQct2S46pc,9p_6pZCYVok,dGYrMh-VhAw,iMF7XbefzXg,HSIaTLgr2bU")
+        id=videoIds)
     response = request.execute()
 
-    #Creates a csv file to store some data from the videos: id, title and view count.
+    # Creates a csv file to store some data from the videos: id, title and view count.
 
     i = 0
-    with open('videos.csv', 'w', newline='') as csv_file:
-        fieldnames = ["Numero","id", "Titulo", "Views"]
+    with open(name, 'w', newline='') as csv_file:
+        fieldnames = ["Numero", "id", "Titulo", "Views"]
         writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
         writer.writeheader()
 
         for data in response['items']:
-            i+=1
-            print(data['id'],data['snippet']['title'],data['statistics']['viewCount']);
-            writer.writerow({"Numero": i, "id": data['id'],"Titulo": data['snippet']['title'], "Views": data['statistics']['viewCount']})
-            # if i == 40:
-            #     writer.writerow({})
-            #     writer.writerow({"Numero": "-", "id":"-","Titulo": "PCO", "Views": "-"})
-            #     writer.writerow({})
-            # if i == 58:
-            #     writer.writerow({})
-            #     writer.writerow({"Numero": "-", "Titulo": "DCTV", "Views": "-"})
-            #     writer.writerow({})
+            i += 1
+            print(data['id'], data['snippet']['title'],
+                  data['statistics']['viewCount'])
+            writer.writerow(
+                {"Numero": i, "id": data['id'], "Titulo": data['snippet']['title'], "Views": data['statistics']['viewCount']})
 
 
 if __name__ == "__main__":
-    main()
+    main(0)
+    main(1)
+    main(2)
+    main(3)
